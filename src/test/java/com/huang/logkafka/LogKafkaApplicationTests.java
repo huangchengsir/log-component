4767b8096ed7e483e2ca93cd122458c3380fba30
huangjiawei <1058403171@qq.com>
2024-12-10 22:52:26 +0800

    update:修改版本

--- src/test/java/com/huang/logkafka/LogKafkaApplicationTests.java
+++ src/test/java/com/huang/logkafka/LogKafkaApplicationTests.java
@@ -12,26 +12,27 @@ import javax.annotation.Resource;
 @SpringBootTest
 class LogKafkaApplicationTests {
 
-    @Autowired
-    LogUtil logUtil;
-    @Resource
-    Metest metest;
+//    @Autowired
+//    LogUtil logUtil;
+//    @Resource
+//    Metest metest;
 
     @Test
     void contextLoads() {
-        LogContent<String, String> test = LogContent.<String, String>builder()
-                .paras("{\"code\":200,\"message\":\"操作成功\",\"data\":{\"object\":[{\"id\":8,\"name\":\"元旦特惠\"")
-                .Key1("测试")
-                .returnValue("null")
-                .logLevel("INFO")
-                .Key3("测试+1")
-                .Key2("阿斯顿撒旦")
-                .stackTrace("")
-                .build();
-        logUtil.Info(test,test);
+//        LogContent<String, String> test = LogContent.<String, String>builder()
+//                .paras("{\"code\":200,\"message\":\"操作成功\",\"data\":{\"object\":[{\"id\":8,\"name\":\"元旦特惠\"")
+//                .Key1("测试")
+//                .returnValue("null")
+//                .logLevel("INFO")
+//                .Key3("测试+1")
+//                .Key2("阿斯顿撒旦")
+//                .stackTrace("")
+//                .build();
+//        logUtil.Info(test,test);
     }
     @Test
     void test(){
-        metest.test();}
+//        metest.test();
+    }
 
 }
